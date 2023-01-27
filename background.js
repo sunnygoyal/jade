ccd68d7fdcdd8c13d4090a977ca0df716eb2caf1
markus <sleepingforge@gmail.com>
2023-01-27 16:30:56 +0100

    Delete open JADE window

--- background.js
+++ background.js
@@ -14,32 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-chrome.runtime.onStartup.addListener(function() {
-  // Find an unused id.
-  // Using an id enables storing the window state, but disables opening multiple windows.
-  // To allow opening multiple-instances, we use an unused id every time. This doesn't
-  // save the state properly, but at-least the first window has consistent state.
-  var usedIds = chrome.window.getAll().map(function(window) {
-    return window.id;
-  });
-  var id = 0;
-  var idStr;
-  do {
-    idStr = "client-" + id;
-    id++;
-  } while (usedIds.indexOf(idStr) > -1)
-
-  var params = {
-	  innerBounds: {
-		  minWidth: 640,
-		  minHeight: 400
-	  },
-      state: "maximized",
-	  id: idStr
-  };
-  chrome.window.create("index.html", params);
-});
-
 chrome.action.onClicked.addListener( () => {
    chrome.tabs.create({url:'index.html'});
 });
